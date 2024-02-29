1c25eb6c7100711892c30798e46a428629cba4c4
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-29 16:01:46 -0400

    Se refactorizo la funcion retornarCantidadItems

--- src/totalizadorVenta.js
+++ src/totalizadorVenta.js
@@ -1,9 +1,8 @@
 function ObtenerCantidadItems(cantidadItems) {
    if (cantidadItems > 0) {
        return cantidadItems;
-   } else {
-       return -1;
    }
+   return -1;
 }
 
 export default ObtenerCantidadItems;
--- src/totalizadorVenta.spec.js
+++ src/totalizadorVenta.spec.js
@@ -8,5 +8,5 @@ describe("Totalizador ventas", () => {
     it("Debería retornar que la cantidad de items comprados es 9 enviendo un parametro a la funcion solo que esta vez la funcion tiene una condicional que verifica que la cantidad de items sea mayor a cero", () => {
         expect(ObtenerCantidadItems(9)).toEqual(9);
     });
-
+    
 });
