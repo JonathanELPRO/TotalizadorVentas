4fc8af4a34228efb03855e03e0329c772909e0d9
Agustin <agustin.ledezma@ucb.edu.bo>
2024-02-29 15:59:09 -0400

    El la funcionalidad se implemento el caso para controlar que la cantidad de items seam mayor a 0

--- src/totalizadorVenta.js
+++ src/totalizadorVenta.js
@@ -1,5 +1,9 @@
-function ObtenerCantidadItems(cantidadItems){
-   return cantidadItems;
+function ObtenerCantidadItems(cantidadItems) {
+   if (cantidadItems > 0) {
+       return cantidadItems;
+   } else {
+       return -1;
+   }
 }
-export default ObtenerCantidadItems
 
+export default ObtenerCantidadItems;
