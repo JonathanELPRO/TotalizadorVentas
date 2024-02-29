5c211cc90e3defe94bc04eda16d511f5a13135ca
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-29 16:00:55 -0400

    Caso de prueba para  obtener la cantidad de items comprados enviando un parametro y viendo una condicion se uso el numero 9

--- src/totalizadorVenta.spec.js
+++ src/totalizadorVenta.spec.js
@@ -5,5 +5,8 @@ describe("Totalizador ventas", () => {
     it("Debería retornar que la cantidad de items comprados es 5 enviendo un parametro a la funcion", () => {
         expect(ObtenerCantidadItems(6)).toEqual(6);
     });
+    it("Debería retornar que la cantidad de items comprados es 9 enviendo un parametro a la funcion solo que esta vez la funcion tiene una condicional que verifica que la cantidad de items sea mayor a cero", () => {
+        expect(ObtenerCantidadItems(9)).toEqual(9);
+    });
 
 });
