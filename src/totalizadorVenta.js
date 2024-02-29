7b8975430d8d67048778b20dd184a7cbb5a4c8a5
Agustin <agustin.ledezma@ucb.edu.bo>
2024-02-29 14:46:24 -0400

    Caso de prueba para Determinar y mostrar el precio total en base a la cantidad y el precio por unidad, como ser el 20x3

---
+++ src/totalizadorVenta.js
@@ -0,0 +1,7 @@
+const cantidadItems = 20;
+const precioPorItem = 3;
+function calcularPrecioNeto(cantidadItems, precioPorItem) {
+    return cantidadItems * precioPorItem;
+  }
+  
+  export default calcularPrecioNeto;
