2d8aa0761ad35da86fdf1eb899e0435e08703699
Agustin <agustin.ledezma@ucb.edu.bo>
2024-02-29 15:44:46 -0400

    Caso de prueba para obtener la cantidad de items con una variable como ser el 6

--- src/totalizadorVenta.spec.js
+++ src/totalizadorVenta.spec.js
@@ -1,7 +1,9 @@
 import retornarCantidadItems from "./totalizadorVenta.js"
 
 describe("Totalizador ventas", () => {
-    it("Debería retornar que la cantidad de items comprados es 5 sin enviar ningún parámetro a la función usada", () => {
-        expect(retornarCantidadItems()).toEqual(5);
+
+    it("Debería retornar que la cantidad de items comprados es 5 enviendo un parametro a la funcion", () => {
+        expect(retornarCantidadItems(6)).toEqual(6);
     });
+
 });
