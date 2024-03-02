30f48bc6b430fa64330eabaabc8c97d9b4dd8a55
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-03-02 10:43:21 -0400

    Se implemento la parte visual de el precio neto y los descuentos por cantidad

--- src/presenter.js
+++ src/presenter.js
@@ -5,18 +5,27 @@ const precioItem = document.querySelector("#precioItem");
 const form = document.querySelector("#totalizador-form");
 const resultadoPrecioNeto = document.querySelector("#resultadoPrecioNeto");
 const resultadoDescuentoPorCantidad = document.querySelector("#resultadoDescuentoPorCantidad");
-
+const resultadoDescuentoPorcentualPorCantidad=document.querySelector("#resultadoDescuentoPorcentualPorCantidad");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const cantidadItemValue = ObtenerCantidadItems(Number.parseInt(cantidadItem.value));
-  const precioItemValue = ObtenerPrecioDeItem(Number.parseInt(precioItem.value));
+  const cantidadItemValue = ObtenerCantidadItems(Number.parseFloat(cantidadItem.value));
+  const precioItemValue = ObtenerPrecioDeItem(Number.parseFloat(precioItem.value));
   if (sonValidosLaCantidadYPrecio(precioItemValue,cantidadItemValue)){
-    resultadoPrecioNeto.innerHTML = "<p>" + obtenerPrecioNeto(cantidadItemValue,precioItemValue) + "</p>";
-    ///console.log(obtenerPrecioNetoAplicandoDescuento(resultadoPrecioNeto.value, ObtenerDescuentos(cantidadItemValue)))
-    resultadoDescuentoPorCantidad.innerHTML = "<p>" + obtenerPrecioNetoAplicandoDescuento(Number.parseFloat(resultadoPrecioNeto.value), ObtenerDescuentos(cantidadItemValue)).toString() + "</p>"; 
-    ///console.log(resultadoDescuentoPorCantidad.value);
+    
+    resultadoPrecioNeto.innerHTML = obtenerPrecioNeto(cantidadItemValue,precioItemValue).toString();
+    console.log(resultadoPrecioNeto.textContent)
+    if (ObtenerDescuentos(cantidadItemValue)!=="No tiene descuento por cantidad"){
+      resultadoDescuentoPorCantidad.innerHTML = obtenerPrecioNetoAplicandoDescuento(Number.parseFloat(resultadoPrecioNeto.textContent),ObtenerDescuentos(cantidadItemValue)).toString() ; 
+      resultadoDescuentoPorcentualPorCantidad.innerHTML=(ObtenerDescuentos(cantidadItemValue)*100).toString()
+    }
+    else
+    {
+      resultadoDescuentoPorCantidad.innerHTML = 0;
+      resultadoDescuentoPorcentualPorCantidad.innerHTML=0;
+    }
+    
     
   }
   else{
